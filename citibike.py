6f82358d353af628cf1b188ef268a83b96403982
rhnptel <rpp257@nyu.edu>
2016-09-27 00:51:34 -0400

    CITIBIKE

---
+++ citibike.py
@@ -0,0 +1,31 @@
+import requests
+from pandas.io.json import json_normalize
+import matplotlib.pyplot as plt
+import sqlite3 as lite
+import time
+from dateutil.parser import parse 
+import collections
+r = requests.get('http://www.citibikenyc.com/stations/json')
+df = json_normalize(r.json()['stationBeanList'])
+import sqlite3 as lite
+con = lite.connect('citi_bike.db')
+cur = con.cursor()
+with con:
+   cur.execute('CREATE TABLE citibike_reference (id INT PRIMARY KEY, totalDocks INT, city TEXT, altitude INT, stAddress2 TEXT, longitude NUMERIC, postalCode TEXT, testStation TEXT, stAddress1 TEXT, stationName TEXT, landMark TEXT, latitude NUMERIC, location TEXT )')
+sql = "INSERT INTO citibike_reference (id, totalDocks, city, altitude, stAddress2, longitude, postalCode, testStation, stAddress1, stationName, landMark, latitude, location) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
+with con:
+   for station in r.json()['stationBeanList']:
+      cur.execute(sql,(station['id'],station['totalDocks'],station['city'],station['altitude'],station['stAddress2'],station['longitude'],station['postalCode'],station['testStation'],station['stAddress1'],station['stationName'],station['landMark'],station['latitude'],station['location']))
+station_ids = df['id'].tolist() 
+station_ids = ['_' + str(x) + ' INT' for x in station_ids]
+with con:
+   cur.execute("CREATE TABLE available_bikes ( execution_time INT, " +  ", ".join(station_ids) + ");")
+exec_time = parse(r.json()['executionTime'])
+with con:
+   cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time.strftime('%s'),))
+id_bikes = collections.defaultdict(int)
+for station in r.json()['stationBeanList']:
+   id_bikes[station['id']] = station['availableBikes']
+with con:
+   for k, v in id_bikes.iteritems():
+      cur.execute("UPDATE available_bikes SET _" + str(k) + " = " + str(v) + " WHERE execution_time = " + exec_time.strftime('%s') + ";")
